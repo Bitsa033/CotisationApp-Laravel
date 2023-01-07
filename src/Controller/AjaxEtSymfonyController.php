ca9cab6499b14cd5e4938e6acc4c4cc1b7ac9d43
Bitsapascal <bitapascal033@gmail.com>
2023-01-07 21:33:47 +0100

    Finalistion de l'insertion et de l'affichage des données avec ajax et symfony.On rafraichi le navigateur avec la methode reload

--- src/Controller/AjaxEtSymfonyController.php
+++ src/Controller/AjaxEtSymfonyController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Repository\ClientRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -15,25 +17,43 @@ class AjaxEtSymfonyController extends AbstractController
     /**
      * @Route("/", name="app_ajax")
      */
-    public function index(): Response
+    public function index(ClientRepository $clientRepository): Response
     {
     
         return $this->render('ajax_et_symfony/index.html.twig', [
             'controller_name' => 'AjaxEtSymfonyController',
+            'clients'=>$clientRepository->findAll()
         ]);
     }
 
     /**
      * @Route("traiter", name="app_traiter")
      */
-    public function traiter(Request $request, SessionInterface $session)
+    public function traiter(Request $request)
     {
-        $nom=$request->get('input');
-        if (empty($nom)) {
-            return new Response('Remplissez vos champs');
+        if (!empty($request->request->get('nom')) && !empty($request->request->get('contact'))) {
+            $nom=$request->request->get('nom');
+            $contact=$request->request->get('contact');
+            //$age=$request->get('age');
+            $client=new Client();
+            $client->setNom($nom);
+            $client->setContact($contact);
+            $m=$this->getDoctrine()->getManager();
+            $m->persist($client);
+            $m->flush();
+            return $this->json([
+                'message'=>'Vos données ont été enregistrer avec success',
+                'icon'=>'alert-success',
+                'nom'=>$nom,
+                'contact'=>$contact
+            ]);
+            
         }
         else {
-            return new Response($nom);
+            return $this->json([
+                'message'=>'Votre formulaire ne doit pas est vide',
+                'icon'=>'alert-error'
+            ]);
         }
     }
     /**
