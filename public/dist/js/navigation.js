c1e0b5aabb48a8fc4675c4422e27c2f0c925485d
Bitsapascal <bitapascal033@gmail.com>
2024-02-07 07:36:45 +0100

    modification du système de navigation

--- public/dist/js/navigation.js
+++ public/dist/js/navigation.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("#menu").click(function() {
+	$(".menu").click(function() {
 		//alert('Jai cliqué')
 		link=$(this).attr("href")
 		$.ajax({
@@ -15,6 +15,24 @@ $(document).ready(function() {
 		})
 		return false
 	})
+
+	$(".home").click(function() {
+		//alert('Jai cliqué')
+		link=$(this).attr("href")
+		$.ajax({
+			url:link,
+			cache: false,
+			success:function(html) {
+				window.location.href=link
+			},
+			error:function(XMLHttpRequest,textStatus,errorThrown) {
+				alert(textStatus)
+			}
+			
+		})
+		return false
+	})
+	
 })
 
 function afficher(data) {
@@ -23,5 +41,15 @@ function afficher(data) {
 		$("#wrapper").append(data)
 		$("#wrapper").fadeIn(500)
 	})
+		
+}
+
+function home(data) {
+	window.location.href=data
+	// $("#wrapper").fadeOut(500,function() {
+	// 	$("#wrapper").empty()
+	// 	$("#wrapper").append(data)
+	// 	$("#wrapper").fadeIn(500)
+	// })
 		
 }
---
+++ src/Controller/CompteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+use App\Services\C2;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompteController extends AbstractController
+{
+    
+
+}
